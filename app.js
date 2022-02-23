60fd358b9c0f04c35ff7aab3afcb186cc664b5dd
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-02-23 00:57:50 -0600

    Agrega requests para alcaldias, privadas y publicas. Arregla alcaldias.json

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const alcaldias = require(__dirname + "/data/alcaldias.json")
+const privadas = require(__dirname + "/data/privadas.json")
+const publicas = require(__dirname + "/data/publicas.json")
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/alcaldias', (req, res) => {
+  res.json(alcaldias)
+})
+
+app.get('/privadas', (req, res) => {
+  res.json(privadas)
+})
+
+app.get('/publicas', (req, res) => {
+  res.json(publicas)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
