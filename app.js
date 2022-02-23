1b79c8a603d36830dcf263b64ab1d4cc72070129
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-02-23 10:19:12 -0600

    Agrega javascript externo en index.html

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"));
+
 const alcaldias = require(__dirname + "/data/alcaldias.json")
 const privadas = require(__dirname + "/data/privadas.json")
 const publicas = require(__dirname + "/data/publicas.json")
