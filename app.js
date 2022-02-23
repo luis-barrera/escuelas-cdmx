5960761381664d9104a5590d65c5711e924a990a
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-02-23 10:55:02 -0600

    Agrega commentarios a app.js

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Hacemos público el dir "public" que contiene js y css para la página
 app.use(express.static("public"));
 
+// Obtenemos los archivos que contienen la información
 const alcaldias = require(__dirname + "/data/alcaldias.json")
 const privadas = require(__dirname + "/data/privadas.json")
 const publicas = require(__dirname + "/data/publicas.json")
 
+// Home
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
+// Rutas para obtener los datos a través de get requests.
 app.get('/alcaldias', (req, res) => {
   res.json(alcaldias)
 })
@@ -24,6 +28,7 @@ app.get('/publicas', (req, res) => {
   res.json(publicas)
 })
 
+// Puerto de la app
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
