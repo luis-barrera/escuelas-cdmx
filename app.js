d0d33b14f1020ae284c6846d36b7007933bdd0d0
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2023-05-05 23:03:13 -0600

    Vercel y Vite

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Hacemos público el dir "public" que contiene js y css para la página
+app.use(express.static("public"));
+
+// Obtenemos los archivos que contienen la información
+const alcaldias = require(__dirname + "/data/alcaldias.json")
+const privadas = require(__dirname + "/data/privadas.json")
+const publicas = require(__dirname + "/data/publicas.json")
+
+// Home
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+// Rutas para obtener los datos a través de get requests.
+app.get('/alcaldias', (req, res) => {
+  res.json(alcaldias)
+})
+
+app.get('/privadas', (req, res) => {
+  res.json(privadas)
+})
+
+app.get('/publicas', (req, res) => {
+  res.json(publicas)
+})
+
+// Puerto de la app
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
